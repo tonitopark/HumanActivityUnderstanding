5d239961c7599ec54bfa34b5ddd4c404928a3653
tonitopark <tony.nokap.park@gmail.com>
2018-12-04 21:19:39 +0900

    test function

--- model_test.py
+++ model_test.py
@@ -1,61 +1,9 @@
-import os
-import json
-
-import torch
-from torch.autograd import Variable
 
 from models.i3d import I3D
 from dataset.videodataset import *
 
 
 from argument_parser import parse_arguments
-import pandas as pd
-
-
-################################################################################
-# Metrics
-################################################################################
-
-def compute_video_hit_at_k(ground_truth, prediction, top_k=3, avg=False):
-    """Compute accuracy at k prediction between ground truth and
-    predictions data frames. This code is greatly inspired by evaluation
-    performed in Karpathy et al. CVPR14.
-
-    Parameters
-    ----------
-    ground_truth : df
-        Data frame containing the ground truth instances.
-        Required fields: ['video-id', 'label']
-    prediction : df
-        Data frame containing the prediction instances.
-        Required fields: ['video-id, 'label', 'score']
-
-    Outputs
-    -------
-    acc : float
-        Top k accuracy score.
-    """
-    video_ids = np.unique(ground_truth['video_id'].values)
-    avg_hits_per_vid = np.zeros(video_ids.size)
-    for i, vid in enumerate(video_ids):
-        pred_idx = prediction['video_id'] == vid
-        if not pred_idx.any():
-            continue
-        this_pred = prediction.loc[pred_idx].reset_index(drop=True)
-        # Get top K predictions sorted by decreasing score.
-        sort_idx = this_pred['score'].values.argsort()[::-1][:top_k]
-        this_pred = this_pred.loc[sort_idx].reset_index(drop=True)
-        # Get labels and compare against ground truth.
-        pred_label = this_pred['label'].tolist()
-        gt_idx = ground_truth['video_id'] == vid
-        gt_label = ground_truth.loc[gt_idx]['label'].tolist()
-        avg_hits_per_vid[i] = np.mean([1 if this_label in pred_label else 0
-                                       for this_label in gt_label])
-        if not avg:
-            avg_hits_per_vid[i] = np.ceil(avg_hits_per_vid[i])
-    return float(avg_hits_per_vid.mean())
-
-
 
 
 if __name__ == '__main__':
@@ -172,49 +120,12 @@ if __name__ == '__main__':
             previous_video_id=video_id
 
 
-        # counter +=1
-        # print(counter)
-        # if counter > 3:
-        #     break
-
         if (idx % 100):
             with open(
                     os.path.join(args.result_path, 'test.json'), 'w') as fp:
                 json.dump(result_buffer, fp)
 
-   # compute top k accuracy
 
-    ground_truth_video_list, ground_truth_label_list = [], []
-    prediction_video_list, prediction_label_list, prediction_score_list=[], [], []
-
-    for video_ids,results in result_buffer['results'].items():
-
-        ground_truth_video_list.append(video_ids)
-        ground_truth_label_list.append(results['ground_truth']['class_name'])
-
-        for pred in results['prediction']:
-            prediction_video_list.append(video_ids)
-            prediction_label_list.append(pred['class_name'])
-            prediction_score_list.append(pred['score'])
-
-
-    ground_truth = pd.DataFrame({'video_id': ground_truth_video_list,
-                                 'label': ground_truth_label_list})
-
-    ground_truth = ground_truth.drop_duplicates().reset_index(drop=True)
-
-    prediction = pd.DataFrame({'video_id': prediction_video_list,
-                               'label': prediction_label_list,
-                               'score': prediction_score_list})
-
-    hit_at_k = compute_video_hit_at_k(ground_truth,
-                                      prediction, top_k=5)
-
-    print(hit_at_k)
-
-    with open(
-            os.path.join(args.result_path, 'test.json'), 'w') as fp:
-        json.dump(result_buffer, fp)
 
 
 
